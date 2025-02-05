c9b4e3d5ddf89f91d75b9a7afcaa26b1652a5c5f
Victor Yang <vyang11@my.bcit.ca>
2025-02-04 16:02:36 -0800

    modified port

--- labs/2/server.js
+++ labs/2/server.js
@@ -19,6 +19,9 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
